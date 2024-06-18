2b44a380762ae7fb76b47ec431808ce37dc03648
Tomek <s16216@pjwstk.edu.pl>
2024-06-18 18:45:45 +0200

    Initialize project, add essential packages, and create initial components

---
+++ pamo-angular-pwa/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  // {path: '',
+  //   runGuardsAndResolvers: "always",
+  //   canActivate: [authGuard],
+  //   children: [
+  //     {path: 'members', component: MemberListComponent},
+  //     {path: 'members/:id', component: MemberDetailComponent},
+  //   ]
+  // },
+
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: HomeComponent,pathMatch: "full"},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ pamo-angular-pwa/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {ToastrModule} from "ngx-toastr";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ToastrModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
